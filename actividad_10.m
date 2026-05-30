6a00b66c420cff842892ef7e58af5c5c441038b5
Marcos Allen <marcosallenmtzc@gmail.com>
2026-05-29 18:56:41 -0600

    Add files via upload

---
+++ actividad_10.m
@@ -0,0 +1,50 @@
+%% Cargar la lista de puntos desde su archivo
+load('puntos_allen.mat');
+waypoints = lista_puntos;
+
+%% Define Vehicle
+R = 0.1;                % Wheel radius [m]
+L = 0.5;                % Wheelbase [m]
+dd = DifferentialDrive(R,L);
+
+%% Simulation parameters
+sampleTime = 0.1;               % Sample time [s]
+tVec = 0:sampleTime:411;         % Time array
+
+initPose = [waypoints(1,1); waypoints(1,2); 0];             % Initial pose (x y theta)
+pose = zeros(3,numel(tVec));    % Pose matrix
+pose(:,1) = initPose;
+
+% Create visualizer
+viz = Visualizer2D;
+viz.hasWaypoints = true;
+
+%% Pure Pursuit Controller
+controller = controllerPurePursuit;
+controller.Waypoints = waypoints;
+controller.LookaheadDistance = 0.4;
+controller.DesiredLinearVelocity = 1;
+controller.MaxAngularVelocity = 20;
+
+%% Simulation loop
+close all
+r = rateControl(1/sampleTime);
+for idx = 2:numel(tVec) 
+    % Run the Pure Pursuit controller and convert output to wheel speeds
+    [vRef,wRef] = controller(pose(:,idx-1));
+    [wL,wR] = inverseKinematics(dd,vRef,wRef);
+ 
+    
+    % Compute the velocities
+    [v,w] = forwardKinematics(dd,wL,wR);
+    velB = [v;0;w]; % Body velocities [vx;vy;w]
+    vel = bodyToWorld(velB,pose(:,idx-1));  % Convert from body to world
+    
+    % Perform forward discrete integration step
+    pose(:,idx) = pose(:,idx-1) + vel*sampleTime; 
+    
+    % Update visualization
+    viz(pose(:,idx),waypoints)
+    waitfor(r);
+    
+end
